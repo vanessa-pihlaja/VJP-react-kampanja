80d4d3b5ee0180c1280c3b02bc442c2a2d0022eb
Vanessa Pihlaja <vanessa.pihlaja@aalto.fi>
2021-05-25 12:05:25 +0300

    muutoksia

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
   import React from "react";
   import "./styles.css";
- 
- 
   import _ from "lodash";
   
-  import {BrowserRouter as Router,Route,Switch, Link} from "react-router-dom" 
-  import Disinformaatio from "./disinformaatio"
-  import Rokottaminen_Suomessa from "./rokottaminen Suomessa"
-  import Tarinoita from "./tarinoita"
-  import Etusivu from "./etusivu"
-  import Navigaatio from "./navigaatio"
-  
+  import {BrowserRouter as Router,Route,Switch } from "react-router-dom";
+  import Disinformaatio from "./disinformaatio";
+  import Rokottaminen_Suomessa from "./rokottaminen Suomessa";
+  import Tarinoita from "./tarinoita";
+  import Etusivu from "./etusivu";
+  import Navigaatio from "./navigaatio";
 
   
   export default function App() {
 
-    
-    
-
     return (
-      <Router>
-        <Navigaatio></Navigaatio>
       
-      <Switch>
-
-        <Route path="/etusivu"><Etusivu/></Route>
-        <Route path="/tarinoita"><Tarinoita/></Route>
-        <Route path="/rokottaminen Suomessa"><Rokottaminen_Suomessa/></Route>
-        <Route path="/disinformaatio"><Disinformaatio/></Route>
-        <Route path="/"></Route>
-      </Switch>
-      
-      </Router>
+      <div>
+        <Router>
+          <Navigaatio/>
+           <div>
+             <Switch>
+              <Route exact path="/"><Etusivu/></Route>
+              <Route exact path="/etusivu"><Etusivu/></Route>
+              <Route exact path="/tarinoita"><Tarinoita/></Route>
+              <Route exact path="/rokottaminen Suomessa"><Rokottaminen_Suomessa/></Route>
+              <Route exact path="/disinformaatio"><Disinformaatio/></Route>
+             </Switch>
+            </div>
+         </Router>
+      </div>
     );
+         
+          
   }
 
-
-
-
-/*export default App;*/
